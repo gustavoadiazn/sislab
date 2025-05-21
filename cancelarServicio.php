bcd12bf542366dcf21f5189fdc881e9293e5a377
gustavoadiazn <gustavoadiazn@gmail.com>
2025-05-21 15:03:25 -0600

    modified:   cancelarServicio.php 	modified:   nuevoServicio.php 	new file:   updServicio.php

--- cancelarServicio.php
+++ cancelarServicio.php
@@ -11,14 +11,32 @@ while ($row2 = $result2->fetch_array()) {
     $idestudio = $row2["estudios_idestudio"];
     $updestudio = "update estudios set total=total+1 where idestudio='$idestudio'";
     $upd = $db->query($updestudio);
+
+    $borrar2 = "delete from datosxestudiosxservicio where estudiosxServicio_idestudiosxServicio in
+    (SELECT idestudiosxServicio FROM estudiosxservicio e where e.servicios_idservicio='$ser' and e.estudios_idestudio='$idestudio');";
+    if ($db->query($sql) === TRUE) {
+        echo "Estudios x Servicio Cancelados exitosamente";
+    } else {
+        echo "Error";
+    }
 }
+
+
 
 $sql = "DELETE FROM estudiosxservicio where servicios_idservicio='$ser'";
 if ($db->query($sql) === TRUE) {
+    echo "Estudios x Servicio Cancelados exitosamente";
+} else {
+    echo "Error";
+}
+
+$sql3 = "DELETE FROM servicios where idservicio='$ser'";
+if ($db->query($sql3) === TRUE) {
     echo "Servicio Cancelado exitosamente";
 } else {
     echo "Error";
 }
+
 $db->close();
 ?>
 <html>
